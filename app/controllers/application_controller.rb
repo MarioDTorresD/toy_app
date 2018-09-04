b9e4c2da19774ad2a6ffa7c587bcbf8dae7af61f
Mario Daniel Torres <mdtorres2809@gmail.com>
2018-09-03 22:32:23 -0500

    Added Placeholder for Toy App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    def placeholder
+        render html: "PLACEHOLDER FOR THE TOY APP!"
+    end
+
 end

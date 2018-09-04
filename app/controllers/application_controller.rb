fe38edc31578aa8c2ca4d0b84c3a55c7ec9fbf85
Mario Daniel Torres <mdtorres2809@gmail.com>
2018-09-03 22:56:07 -0500

    Added Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    def placeholder
-        render html: "PLACEHOLDER FOR THE TOY APP!"
+    def homepage
+        render html: "WELCOME TO MY TOY APP!"
     end
 
 end
